50c0ea87907ede5db7c04c5be76cf36ac693b2f1
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2022-06-11 23:05:47 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+const form = document.getElementById("create-task-form");
+const inputValue = document.getElementById("new-task-description");
+const listBody = document.getElementById("list");
+const tasksBody = document.getElementById("tasks");
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener("submit", handleSubmit);
 });
+
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const list = document.createElement("li");
+  list.innerText = `${inputValue.value}`;
+  tasksBody.appendChild(list);
+
+  form.reset();
+}
+
+
+
